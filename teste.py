799415c380110015e0a15043fe44b0fe3db62888
Gustavo <128264896+Gustavo-maia-gst@users.noreply.github.com>
2023-11-03 12:28:00 -0300

    Deleção de alguns arquivos

--- teste.py
+++
@@ -1,7 +0,0 @@
-from csvmanager import *
-
-with CSVManager('file.csv') as csv:
-    print(csv.table.get())
-    csv.table.add(['Lenner', 18, 'CC', 1])
-    print(csv.table.get())
-    print(csv.table.get(filters={'idade': 17}))
